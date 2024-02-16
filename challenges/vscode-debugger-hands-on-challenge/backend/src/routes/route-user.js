f440b4070bd9c1520f84e6eaf2831aa7beed8461
David Freitas <davidfreitasqa@gmail.com>
2024-02-16 20:38:50 -0300

    vscode-debugger-hands-on-challenge

---
+++ challenges/vscode-debugger-hands-on-challenge/backend/src/routes/route-user.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { getAllUsersFromDatabase, insertUserIntoDatabase } = require('../modules/databaseOperations.js');
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await getAllUsersFromDatabase(req.connection, re);
+        if (result.length === 0) {
+            res.status(404).json({msg: "Not found."});
+        } else {
+            res.send(result);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error Internal Server.');
+    }
+});
+
+router.post('/insertUsers',  async (req, res) => {
+    const { username, score, round, level } = req.body;
+    const browser = req.useragent.browser;
+    const os = req.useragent.os;
+    const ip_address = req.clientIp;
+
+    const user = { username, score, round, level, os, browser, ip_address };
+
+    try {
+        await insertUserIntoDatabase(req.connection, user, res);
+        res.status(200).json({msg: 'Registration Entered Successfully'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg: 'Error Internal Server.'});
+    }
+});
+
+module.exports = router;
