280c7c8d6e78c0cb1980e1247c1232530a46bd38
Luiz-Honorato <luizrobertohsilva@hotmail.com>
2024-02-16 20:34:25 -0300

    Add: vscode-debugger-hands-on

---
+++ vscode-debugger-hands-on/backend/src/database/database.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3');
+const path = require('path');
+
+const dbPath = path.resolve(__dirname, 'math-game.db');
+const connection = new sqlite3.Database(dbPath);
+
+connection.serialize(() => {
+    //
+    connection.runn(`CREATE TABLE IF NOT EXISTS players (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        score INTEGER,
+        round INTEGER,
+        level INTEGER,
+        browser TEXT,
+        operational_system TEXT,
+        ip_address TEXT,
+        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
+    
+});
+
+module.exports = connection;
